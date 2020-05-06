8439d396c4d5fcd5e67c96ed6924d92c984787a9
Yan Yan <yan.z.yan@helsinki.fi>
2020-05-06 11:23:26 +0300

    change of structure

--- src/app/controller.go
+++ src/app/controller.go
@@ -9,7 +9,7 @@ import (
 
 func registerRoutes() *gin.Engine {
 	r := gin.Default()
-	r.LoadHTMLGlob("../../templates/**/*.html")
+	r.LoadHTMLGlob("templates/**/*.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
@@ -92,7 +92,7 @@ func registerRoutes() *gin.Engine {
 		}
 	})
 
-	r.Static("../../public", "./public")
+	r.Static("/public", "./public")
 
 	return r
 }
